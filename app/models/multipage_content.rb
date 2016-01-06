08bfd60451e0cca304cdc40a6a31874b076c6af5
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-01-06 12:09:12 +0000

    Add a base multipage content model...
    This is the base model for various multiple page formats which contain
    content parts particular to each page.

---
+++ app/models/multipage_content.rb
@@ -0,0 +1,47 @@
+class MultipageContent
+
+  attr_reader :parts, :content_id, :base_path, :title, :description, :public_updated_at
+
+  def initialize(attrs)
+    attrs = attrs.deep_symbolize_keys
+    @content_id = attrs.fetch(:content_id)
+    @base_path = attrs.fetch(:base_path)
+    @title = attrs.fetch(:title)
+    @description = attrs.fetch(:description)
+    details = attrs.fetch(:details)
+    @public_updated_at = details.fetch(:public_updated_at)
+
+    assign_parts(details)
+  end
+
+  def part?(slug)
+    parts.find{ |p| p.slug == slug }.present?
+  end
+
+private
+
+  def assign_parts(details)
+    return unless details.key?(:parts)
+    @parts = details[:parts].map { |part| Part.new(part) }
+  end
+end
+
+class Part
+  attr_reader :slug, :title, :body
+
+  def initialize(attrs)
+    @slug = attrs.fetch(:slug)
+    @title = attrs.fetch(:title)
+    @body = attrs.fetch(:body)
+  end
+
+  # TODO: Presenter method?
+  def self.group_size(parts)
+    size = parts.length.to_f/2
+    if size < 2
+      3
+    else
+      size.ceil
+    end
+  end
+end
---
+++ spec/models/multipage_content_spec.rb
@@ -0,0 +1,50 @@
+require "rails_helper"
+
+RSpec.describe MultipageContent do
+  describe "initialize" do
+    let(:content_id) { SecureRandom.uuid }
+    let(:details) do
+      {
+        "public_updated_at" => "2015-10-15T11:00:20+01:00",
+        "parts" => [
+          {"title" => "Part one", "slug" => "part-one", "body" => "A new beginning"},
+          {"title" => "Part two", "slug" => "part-two", "body" => "The next bit"},
+        ]
+      }
+    end
+    let(:attrs) do
+      {
+        "base_path" => "/foo/bar",
+        "title" => "A two part guide to foobar",
+        "description" => "What follows is a brief description...",
+        "content_id" => content_id,
+        "not_readable" => "not readable",
+        "details" => details,
+      }
+    end
+    subject { described_class.new(attrs) }
+
+    it "assigns each attribute" do
+      expect(subject.content_id).to eq(attrs["content_id"])
+      expect(subject.base_path).to eq(attrs["base_path"])
+      expect(subject.title).to eq(attrs["title"])
+      expect(subject.description).to eq(attrs["description"])
+    end
+
+    it "flattens details attributes" do
+      expect(subject.public_updated_at).to eq("2015-10-15T11:00:20+01:00")
+    end
+
+    it "assigns parts" do
+      expect(subject.parts.size).to eq(2)
+
+      expect(subject.parts.first.slug).to eq("part-one")
+      expect(subject.parts.first.title).to eq("Part one")
+      expect(subject.parts.first.body).to eq("A new beginning")
+
+      expect(subject.parts.second.slug).to eq("part-two")
+      expect(subject.parts.second.title).to eq("Part two")
+      expect(subject.parts.second.body).to eq("The next bit")
+    end
+  end
+end
