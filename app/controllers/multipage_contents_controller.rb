857d199940e9f6ea2e5b7c40372ece55084e6651
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-01-06 12:32:57 +0000

    Add a base multipage content controller...
    Handles the common redirecting and rendering of content divided into
    parts.

---
+++ app/controllers/multipage_contents_controller.rb
@@ -0,0 +1,31 @@
+class MultipageContentsController < ApplicationController
+  def show
+    content_item_response = ContentStore.service.content_item(base_path)
+    @content = model_class.new(content_item_response.to_hash) if content_item_response
+
+    if @content.present?
+      if params[:part]
+        @current_part = current_part
+        redirect_to base_path unless @current_part
+      end
+    else
+      render_404
+    end
+  end
+
+private
+
+  def model_class
+    controller_name.classify.constantize
+  end
+
+  def base_path
+    "/#{params[:slug]}"
+  end
+
+  def current_part
+    return unless part_slug = params[:part]
+    return unless @content.parts && @content.parts.any?
+    @content.parts.find{ |part| part.slug == part_slug }
+  end
+end
--- app/controllers/multipart_contents_controller.rb
+++
@@ -1,6 +0,0 @@
-class MultipartContentsController < ApplicationController
-  def show
-    @content = ContentStore.service.content_item("/#{params[:slug]}")
-    render_404 unless @content.present?
-  end
-end
